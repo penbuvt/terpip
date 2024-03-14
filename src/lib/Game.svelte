cae4c4b69d5ba0bf81e9bbbe1b27ee40a5ed2511
Penbu <penbu@penbuvt.ca>
2024-03-13 22:00:34 -0400

    Handle clicks on game board
    A click on a cell's button will register a move.

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -3,6 +3,14 @@
   import { Game } from './terpip-engine';
 
   let game = new Game();
+
+  function handlePlayerMove({ detail: { row, col }}) {
+    game.move(row, col);
+    game = game;
+  }
 </script>
 
-<Grid data={game.state.board} />
+<Grid
+  data={game.state.board}
+  on:playerMove={handlePlayerMove}
+/>
