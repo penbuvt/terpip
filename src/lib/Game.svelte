371ee7b4bc99efbcd5e3ec98f6a88289d36dfbee
Penbu <penbu@penbuvt.ca>
2024-03-13 21:51:41 -0400

    Render game on page

---
+++ src/lib/Game.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Grid from './Grid.svelte';
+  import { Game } from './terpip-engine';
+
+  let game = new Game();
+</script>
+
+<Grid data={game.state.board} />
---
+++ src/lib/terpip-engine/index.ts
@@ -0,0 +1,2 @@
+export type * from './game-state.ts';
+export * from './game.js';
